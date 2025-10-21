9a5a7a1653f418f72b146cfb5bbb96eddde20f3b
Shine Ncube <ncubeshine376@gmail.com>
2025-10-21 12:06:24 +0200

    changed applogo with the blue background to the one with 3 people

---
+++ village savings app/home page.swift
@@ -0,0 +1,18 @@
+//
+//  home page.swift
+//  village savings app
+//
+//  Created by Shine Ncube on 10/17/25.
+//
+
+import SwiftUI
+
+struct home_page: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    home_page()
+}
