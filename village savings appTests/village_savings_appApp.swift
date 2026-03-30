74fdb047971913189f62f78a5f319cc5b761d818
Shine <ncubeshine376@gmail.com>
2026-03-30 11:13:12 +0200

    added .environmentObject and added @ stateObject

--- village savings appTests/village_savings_appApp.swift
+++ village savings appTests/village_savings_appApp.swift
@@ -9,9 +9,12 @@ import SwiftUI
 
 @main
 struct village_savings_appApp: App {
+    @StateObject private var appData = AppData()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(appData)
         }
     }
 }
